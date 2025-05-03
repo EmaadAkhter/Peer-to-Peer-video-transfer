5988e1a33401f5da789eca19c71e861f51a88275
Emaad_Ansari <67855951+EmaadAkhter@users.noreply.github.com>
2025-05-03 14:50:58 +0530

    Update FileInput.tsx

--- code/src/components/FileInput.tsx
+++ code/src/components/FileInput.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 
 interface Props {
-  onFileSelected: (file: File | null) => void;
+  onFilesSelected: (files: FileList | null) => void;
 }
 
-const FileInput: React.FC<Props> = ({ onFileSelected }) => (
+const FileInput: React.FC<Props> = ({ onFilesSelected }) => (
   <div style={{ marginTop: '1rem' }}>
-    <input 
-      type="file" 
+    <input
+      type="file"
       accept="video/webm"
-      onChange={(e) => {
-        const file = e.target.files?.[0];
-        if (file && !file.type.startsWith('video/webm')) {
-          alert('Please select a WebM video file');
-          return;
-        }
-        onFileSelected(file || null);
-      }} 
+      multiple
+      onChange={e => onFilesSelected(e.target.files)}
     />
     <p style={{ fontSize: '0.8em', color: '#666' }}>
-      Please select a WebM video file
+      Please select one or more WebM video files
     </p>
   </div>
 );
